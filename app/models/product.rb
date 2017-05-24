cf66ee5aa51ac1eda258629740239f239cbed314
Fraz Ahsan <fraz.ahsan17@gmail.com>
2017-05-24 16:22:52 +0500

    Admin panel added

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,13 @@
 class Product < ApplicationRecord
+
+	BASE_URL = "http://suavistech.com:3001"
 	validates_presence_of :price, :name
 	has_attached_file :image, default_url: "/assets/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
   def as_json(options = { })
     h = super(options.merge({ except: [:created_at, :updated_at, :image_file_size, :image_content_type, :image_file_name, :image_updated_at] }))
-    h[:image]   = "http://suavistech.com:3001/"+self.image.url
+    h[:image]   = BASE_URL+self.image.url
     h
   end
 end
